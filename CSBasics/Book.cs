0946071df2196da1dd3d925d14d8a89486786d1c
Imad Ahmed <j166a775433@gmail.com>
2026-03-12 09:18:54 +0000

    Add TurnPage method

---
+++ CSBasics/Book.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace CSBasics
+{
+    internal class Book
+    {
+        public string Title
+        {
+            get;
+            set;
+        }
+        public int Pages
+        {
+            get;
+            set;
+        }
+        public int CurrentPage
+        {
+            get;
+            set;
+        }
+
+        public Book(string title, int pages)
+        {
+            Title = title;
+            Pages = pages;
+            CurrentPage = 1;
+        }
+
+        public void TurnPage()
+        {
+            if (doesPageExist())
+            {
+                CurrentPage++;
+                Console.WriteLine($"The current page is {CurrentPage}");
+            } else
+            {
+                Console.WriteLine("The new page does not exist!");
+            }
+
+               
+        }
+
+        private bool doesPageExist()
+        {
+            return CurrentPage <= Pages;
+        }
+    }
+}
--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Book book1 = new Book("My Book", 200);
+            book1.CurrentPage = 50;
+            book1.TurnPage();
+
+            //Console.WriteLine("Book title: " + book1.Title);
+            //Console.WriteLine("Current page: " + book1.CurrentPage);
+
+
+
+
+            //double pi = 3.142;
+            //Console.WriteLine(pi);
+
+            //bool isLearningCs = true;
+            //Console.WriteLine(isLearningCs);
+
+            //string favouriteFilm = "Terminator 2";
+            //Console.WriteLine("My favourite film is " + favouriteFilm);
+
+            //string firstName = "Imad";
+            //printGreeting(firstName);
+
+            //Console.WriteLine(SubtractValue(10, 2));
+
+            //Console.WriteLine(isStringLonger("Hello", 6));
+
+            //int subtractionResult = SubtractValue(20, 8);
+            //Console.WriteLine(subtractionResult);
+
         }
-    }
+
+        //private static void printGreeting(string name)
+        //{
+        //    Console.WriteLine("Hello " + name);
+        //}
+
+        //private static int SubtractValue(int firstNumber, int secondumber)
+        //{
+        //    return firstNumber - secondumber;
+        //}
+
+        //private static bool isStringLonger(string word, int number)
+        //{
+        //    return word.Length > number;
+        //}
+}
 }
